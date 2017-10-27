e9ecf2991e82bb8b80fb5a37e463118361f28d9b
unknown <lynam.chris@gmail.com>
2017-10-27 13:30:12 -0500

    added basic firebase connectivity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// set global variables
 var playerOneChoice;
 var playerTwoChoice = "rock";
 var playerOneWins = 0;
@@ -6,12 +7,44 @@ var playerOneLosses = 0;
 var playerTwoLosses = 0;
 
 
-// Listen for player one play
-$(".p1").on("click", function(){
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBxkiSDl4QQdH1lQk_j7DhwdoNPc-Z86io",
+	authDomain: "rps-multiplayer-96045.firebaseapp.com",
+	databaseURL: "https://rps-multiplayer-96045.firebaseio.com",
+	projectId: "rps-multiplayer-96045",
+	storageBucket: "rps-multiplayer-96045.appspot.com",
+	messagingSenderId: "481414795010"
+};
 
-	playerOneChoice = $(this).attr("data-one");
+firebase.initializeApp(config);
 
-	if (playerOneChoice === playerTwoChoice) {
+var database = firebase.database();
+
+// setup player in Firebase
+$("#player-submit").on("click", function(event) {
+        
+        event.preventDefault();
+
+        var player = $("#player-name").val().trim();
+
+        database.ref().set({
+        	players:{
+        		1:{
+        			losses: 0,
+        			name: player,
+        			wins: 0
+        		}
+        	}
+      	});
+
+        $('#player-display').html("Hi "+player+"! You are player 1.");
+      });
+
+
+// Function to determine winner
+function determineWinner() {
+		if (playerOneChoice === playerTwoChoice) {
 		$("#results").html("It was a tie!");
 	}
 
@@ -53,6 +86,26 @@ $(".p1").on("click", function(){
             playerTwoLosses++;
         }
     }
+
+    database.ref("players/1").update({
+    	losses: playerOneLosses,
+    	wins: playerOneWins,
+    	// losses: playerTwoLosses,
+    	// wins: playerTwoWins,
+    })
+
+    database.ref("players/2").update({
+    	losses: playerTwoLosses,
+    	wins: playerTwoWins,
+    })
+};
+
+// Listen for player one play
+$(".p1").on("click", function(){
+
+	playerOneChoice = $(this).attr("data-one");
+
+	determineWinner();
 
     $("#score-one").html("Wins: "+playerOneWins+" Losses: "+playerOneLosses);
     $("#score-two").html("Wins: "+playerTwoWins+" Losses: "+playerTwoLosses);
